b17cd920ac6730467ecb909d626f4e0d05cc0647
allisonRabia <09snippet-frazzle@icloud.com>
2025-01-13 12:59:22 -0600

    autograde

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -28,7 +28,7 @@ public class SearchController {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
-//    private static ArrayList<Job> jobs = new ArrayList<>();
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping (value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
